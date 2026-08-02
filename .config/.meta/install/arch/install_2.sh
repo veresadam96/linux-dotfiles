38c8c3fbee5d2b2db17a78db492a0631ca95f9cb
veresadam96 <veresadam96@protonmail.com>
2026-08-02 20:56:12 +0200

    install scripts: fwupd

--- .config/.meta/install/arch/install_2.sh
+++ .config/.meta/install/arch/install_2.sh
@@ -20,7 +20,7 @@ packages() {
 	#kernel
 	packages+=("base-devel" "linux-lts");
 	#utilities
-	packages+=("sudo" "ripgrep" "fd" "zip" "unzip" "unrar" "fzf" "bat" "fastfetch" "fuse" "atool" "tmux" "usbutils" "jq" "htop" "libinput" "libinput-tools");
+	packages+=("sudo" "ripgrep" "fd" "zip" "unzip" "unrar" "fzf" "bat" "fastfetch" "fuse" "atool" "tmux" "usbutils" "jq" "htop" "libinput" "libinput-tools" "fwupd");
 	#CD/DVD
 	packages+=("cdrtools" "cdrdao" "libcdio" "libdvdread" "libdvdcss" "libdvdnav");
 	#manual
--- .config/.meta/install/artix/openrc/install_2.sh
+++ .config/.meta/install/artix/openrc/install_2.sh
@@ -31,7 +31,7 @@ packages() {
 	#kernel
 	packages+=("base-devel" "linux-lts");
 	#utilities
-	packages+=("sudo" "ripgrep" "fd" "zip" "unzip" "unrar" "fzf" "bat" "fastfetch" "fuse" "atool" "tmux" "usbutils" "jq" "cronie" "ntp" "htop" "libinput" "libinput-tools");
+	packages+=("sudo" "ripgrep" "fd" "zip" "unzip" "unrar" "fzf" "bat" "fastfetch" "fuse" "atool" "tmux" "usbutils" "jq" "cronie" "ntp" "htop" "libinput" "libinput-tools" "fwupd");
 	#CD/DVD
 	packages+=("cdrtools" "cdrdao" "libcdio" "libdvdread" "libdvdcss" "libdvdnav");
 	#manual
